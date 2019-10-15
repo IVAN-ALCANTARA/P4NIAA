8b8fd98fd80421dc13fca0f8bc81643c6040c44d
ISC Tesoem <tesoem@iMac-Tesoem-21.local>
2019-10-15 15:34:37 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/niaa/p4niaa/MainActivity.java
@@ -0,0 +1,30 @@
+package mx.edu.tesoem.isc.niaa.p4niaa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText vnombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vnombre=(EditText) findViewById(R.id.txtnombre);
+    }
+
+    public void EnviarParametros(View v)
+    {
+
+        Intent pantalla = new Intent(this, RecibeParametrosActivity.class);
+                pantalla.putExtra("pnombre", vnombre.getText().toString());
+                startActivity(pantalla);
+                finish();
+    }
+
+}
